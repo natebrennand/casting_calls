7699a7d962e134874d52e3f1a15c20b091931f38
Jason Pearson <jason.d.pearson@gmail.com>
2013-07-20 22:23:52 -0400

    removed old files

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-require({
-  paths: {
-    'jquery'       : 'vendor/jquery',
-    'underscore'   : 'vendor/underscore',
-    'angular'      : 'vendor/angular',
-    'ui-bootstrap' : 'vendor/ui-bootstrap-tpls'
-  }
-}, ['require', 'bootstrap']);
