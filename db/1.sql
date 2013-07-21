c26c61b15687c2467cb0ed30498befd88bf8740d
AMR- <rotha@seas.upenn.edu>
2013-07-20 22:28:13 -0400

    finished script to create schema, also added script that deletes most tables

--- db/1.sql
+++ db/1.sql
@@ -1,40 +1,92 @@
+use Casting
 
 CREATE TABLE user (
-	id int constraint not null  AUTO_INCREMENT primary key,
-	username varchar(30) not null constraint unique
-	email varchar(100) not null
+	id int not null  AUTO_INCREMENT primary key,
+	username varchar(30) not null unique,
+	email varchar(100) not null,
 	password varchar(100) not null
-)
+);
 
 CREATE TABLE actor (
-	id int constraint not null AUTO_INCREMENT primary key
-	foreign key (userid) REFERENCES user(id) not null
-	gender varchar(6) not null
-	race varchar(30) null
-	height float null
-	weight float null
-	bodytype float null
-)
+	id int not null primary key,
+	FOREIGN KEY fk_user(id) REFERENCES user(id),
+	gender varchar(6) not null,
+	race varchar(30) null,
+	height float null,
+	weight float null,
+	bodytype float null,
+	preferredLocation varchar(5) null,
+	TheatreFlag enum('T', 'F') null,
+	FilmFlag enum('T', 'F') null,
+	MusicalTheatreFlag enum('T', 'F') null,
+	SingingTypes varchar(10) null,
+	SingingYears int null,
+	GymnasticFlag enum('T', 'F') null,
+	CompensationDesired varchar(30)
+);
 
 CREATE TABLE actor_photos (
-	    
-)
-
-
-CREATE TABLE actor_audition
+	id int not null AUTO_INCREMENT primary key,
+	actorid int not null,
+	foreign key fk_actor(actorid) REFERENCES actor(id),
+	dataurl varchar(255)
+);
 
   -- collection of skill fields and attributes
 
-CREATE TABLE company
-CREATE TABLE production
-CREATE TABLE part
-CREATE TABLE audition
-CREATE TABLE cast
+CREATE TABLE company(
+	id int not null primary key,
+	foreign key fk_company(id) REFERENCES user(id),
+	name varchar(30),
+	type varchar(30)
+);
 
+CREATE TABLE production(
+	id int not null AUTO_INCREMENT primary key,
+	companyid int not null,
+	foreign key fk_company(companyid) REFERENCES company(id),
+	name varchar(30),
+	TheatreFlag enum('T', 'F') null,
+	FilmFlag enum('T', 'F') null,
+	MusicalTheatreFlag enum('T', 'F') null
+);
 
--- --  LOW PRIORITY
+CREATE TABLE part(
+	id int not null AUTO_INCREMENT primary key,
+	productionid int not null,
+	foreign key fk_production(productionid) REFERENCES production(id),
+	name varchar(30),
+	gender varchar(6) not null,
+	race varchar(30) null,
+	height float null,
+	weight float null,
+	bodytype float null,
+	SingingType varchar(10) null,
+	GymnasticFlag enum('T', 'F') null,
+	Compensation varchar(30)
+);
 
--- CREATE TABLE actor_alerts
+CREATE TABLE audition(
+	id int not null AUTO_INCREMENT primary key,
+	productionid int not null,
+	foreign key fk_production1(productionid) REFERENCES production(id),
+	address varchar(200) null,
+	latitude varchar(30) null,
+	longitude varchar(30) null
+);
 
--- CREATE TABLE sms_messages
--- CREATE TABLE sms_queue
+CREATE TABLE auditionPart(
+    id int not null AUTO_INCREMENT primary key,
+    partid int not null,
+    auditionid int not null,
+    foreign key fk_production2(partid) REFERENCES part(id),
+    foreign key fk_audition1(auditionid) REFERENCES audition(id) 
+);
+
+CREATE TABLE actor_audition(
+    id int not null AUTO_INCREMENT primary key,
+    actorid int not null,
+    auditionid int not null,
+    foreign key fk_actor2(actorid) REFERENCES actor(id),
+    foreign key fk_audition2(auditionid) REFERENCES audition(id) 
+);
---
+++ db/del.sql
@@ -0,0 +1,11 @@
+use Casting
+
+drop Table auditionPart;
+drop Table audition;
+drop Table part;
+drop Table production;
+drop Table company;
+drop Table actor_photos;
+drop Table actor;
+drop Table user;
+
