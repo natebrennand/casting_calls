029724451f0026a2878b365dd3e4b3e53f67b720
Jason Pearson <jason.d.pearson@gmail.com>
2013-07-21 00:44:42 -0400

    refactored auth controllers

--- static/js/controllers/logout.js
+++ static/js/controllers/logout.js
@@ -1,13 +1,7 @@
 define(['app', 'controllers/http'], function() {
-  window.Logout = function($scope, $http) {
-    $scope.send = function() {
-      var request;
-      request = {
-        method: "POST",
-        url: "/logout"
-      };
-      $http(request).success(refresh).error(showError);
-      dialog.close();
+  window.Logout = function($scope, Accounts) {
+    $scope.request = function() {
+      Accounts.logout(refresh);
     };
   };
 });
